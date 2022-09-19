570c8c0281487d4c431ded768d2594f34762b600
Mônica Zungalo Quintal <111545468+monicaquintal@users.noreply.github.com>
2022-09-19 20:11:05 -0300

    Add files via upload
    Programa desenvolvido em Java, realizado na Agenda 03 do Curso Técnico de Desenvolvimento de Sistemas da ETEC.

---
+++ MensagemSucesso.java
@@ -0,0 +1,12 @@
+package mensagemsucesso;
+
+
+public class MensagemSucesso {
+public static void main(String[] args) {
+        String mensagemSucesso;
+        mensagemSucesso = "Programa instalado com sucesso e em pleno funcionamento!";
+        
+        System.out.println(mensagemSucesso);
+    }
+    
+}
